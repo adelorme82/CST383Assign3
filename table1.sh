7a1f07bb1d9db204eb1418e53f4cee180c5c0fdf
Alex Molina <alexalex1432@gmail.com>
2014-02-13 15:30:02 -0700

    fixing austins stuff

--- table1.sh
+++ table1.sh
@@ -4,21 +4,21 @@ declare -a Tamb
 declare -a Tref
 declare -a Tm
 
-Date[0] = 2/19/2001
-Date[1] = 2/19/2001
-Date[2] = 2/19/2001
-Time[0] = 9:59
-Time[1] = 9:59
-Time[2] = 9:59
-Tamb[0] = 18.2
-Tamb[1] = 18.2
-Tamb[2] = 18.2
-Tref[0] = 25.2
-Tref[1] = 25.2
-Tref[2] = 25.4
-Tm[0] = 22.0
-Tm[1] = 23.7
-Tm[2] = 25.2
+Date[0]="2/19/2001"
+Date[1]="2/19/2001"
+Date[2]="2/19/2001"
+Time[0]="9:59"
+Time[1]="9:59"
+Time[2]="9:59"
+Tamb[0]="18.2"
+Tamb[1]="18.2"
+Tamb[2]="18.2"
+Tref[0]="25.2"
+Tref[1]="25.2"
+Tref[2]="25.4"
+Tm[0]="22.0"
+Tm[1]="23.7"
+Tm[2]="25.2"
 
 declare -a Irradiande
 Irradiande[0]="1013.0"

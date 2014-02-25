7324c930775c70c2e89d9c538837bcf84bf3b29c
Alex Molina <alexalex1432@gmail.com>
2014-02-25 16:05:37 -0700

    Purely aesthetic changes, moving declarations and junk

--- table1.sh
+++ table1.sh
@@ -21,12 +21,19 @@ getUserInput()
 
 }
 
-
 declare -a Date
 declare -a Time
 declare -a Tamb
 declare -a Tref
 declare -a Tm
+declare -a Irradiande
+declare -a Isc
+declare -a Voc
+declare -a Imp
+declare -a Vmp
+declare -a Pm
+declare -a FF
+declare -a answers
 
 Date[0]="2/19/2001"
 Date[1]="2/19/2001"
@@ -43,43 +50,27 @@ Tref[2]="25.4"
 Tm[0]="22.0"
 Tm[1]="23.7"
 Tm[2]="25.2"
-
-declare -a Irradiande
 Irradiande[0]="1013.0"
 Irradiande[1]="1015.0"
 Irradiande[2]="1016.9"
-
-declare -a Isc
 Isc[0]="4.94"
 Isc[1]="4.93"
 Isc[2]="4.95"
-
-declare -a Voc
 Voc[0]="22.30"
 Voc[1]="22.12"
 Voc[2]="22.00"
-
-declare -a Imp
 Imp[0]="4.40"
 Imp[1]="4.48"
 Imp[2]="4.42"
-
-declare -a Vmp
 Vmp[0]="17.70"
 Vmp[1]="17.15"
 Vmp[2]="17.29"
-
-declare -a Pm
 Pm[0]="77.94"
 Pm[1]="76.85"
 Pm[2]="76.34"
-
-declare -a FF
 FF[0]="70.8"
 FF[1]="70.4"
 FF[2]="70.1"
-
-declare -a answers
 
 getUserInput 
 

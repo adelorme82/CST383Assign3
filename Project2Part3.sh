7b9682cef0a99dc5df229cd2436ef2aa0cfdb58e
mrrcrowley <lcoshii@gmail.com>
2014-03-26 18:07:42 -0700

    Finished grep command

--- Project2Part3.sh
+++ Project2Part3.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 
+# need to get loop to function 
+
 for file in *
 do
- grep -v 'beggining[A-Z][A-Z]end'
+ grep -v '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][A-Z][A-Z]'
 done
 
 
